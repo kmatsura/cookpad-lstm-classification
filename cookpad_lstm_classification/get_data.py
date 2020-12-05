16e36f5ea188533a4139bf42f54b0bfc1594ac22
kmatsuuraHMC <bcdearth@gmail.com>
2020-12-05 17:47:56 +0900

    Improve: リファクタリング

--- cookpad_lstm_classification/get_data.py
+++ cookpad_lstm_classification/get_data.py
@@ -5,6 +5,9 @@ from dotenv import load_dotenv
 
 
 def get_title_and_category(conn):
+    """
+    cookpad_dataからレシピ名とそれに対応するカテゴリを持ってくる。
+    """
     cur = conn.cursor()
     category_id = 'c910ca8994e8f6953e0b85bbfc73f8305274886d'
     sql = "SELECT r.title, sc.title FROM recipes r INNER JOIN search_category_recipes scr ON r.id = scr.recipe_id INNER JOIN search_categories sc ON scr.search_category_id = sc.id;"
@@ -13,7 +16,7 @@ def get_title_and_category(conn):
     return result, rows
 
 
-if __name__ == "__main__":
+def main():
     load_dotenv(verbose=True)
     conn = mydb.connect(
         host=os.environ.get("HOST_NAME"),
@@ -30,11 +33,16 @@ if __name__ == "__main__":
     for i, row in enumerate(rows):
         title, category_name = row
         tmp_dict[i] = title, category_name  # df.append()は遅いので、dictを使う。
-    datasets = pd.DataFrame.from_dict(tmp_dict, orient='index', columns=["title", "category"])
-    datasets = datasets.sample(frac=1).reset_index(drop=True) # データフレームシャッフル
+    datasets = pd.DataFrame.from_dict(
+        tmp_dict, orient='index', columns=["title", "category"])
+    datasets = datasets.sample(frac=1).reset_index(drop=True)  # データフレームシャッフル
     BASEDIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     OUTPUTDIR = os.path.join(BASEDIR, 'data/reciepe_category/')
     FILENAME = "recipe_category_datasets.csv"
     if not os.path.exists(OUTPUTDIR):
         os.makedirs(OUTPUTDIR)
     datasets.to_csv(os.path.join(OUTPUTDIR, FILENAME))
+
+
+if __name__ == "__main__":
+    main()
