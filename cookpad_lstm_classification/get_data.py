982c765615cd8e992eb6a08e65f01d922e1894ab
kmatsuuraHMC <bcdearth@gmail.com>
2020-12-05 12:22:52 +0900

    Add: データベースとの接続部分

--- cookpad_lstm_classification/get_data.py
+++ cookpad_lstm_classification/get_data.py
@@ -1,11 +1,24 @@
+import os
 import mysql.connector as mydb
+from dotenv import load_dotenv
 
 
 if __name__ == "__main__":
+    load_dotenv(verbose=True)
     conn = mydb.connect(
-        host='hostname',
-        port='3306',
-        user='kmatsuura',
-        password='password',
-        database='dbname'
+        host=os.environ.get("HOST_NAME"),
+        user=os.environ.get("USER_NAME"),
+        password=os.environ.get("PASSWORD"),
+        database=os.environ.get("DB_NAME")
     )
+    conn.ping(reconnect=True)
+    print("connection:", conn.is_connected())
+    cur = conn.cursor()
+    category_id = 'c910ca8994e8f6953e0b85bbfc73f8305274886d'
+    sql = "select id from search_categories;"
+    result = cur.execute(sql)
+    rows = cur.fetchall()
+    if result == 0:
+        print("No Data")
+    for row in rows:
+        print(row)
